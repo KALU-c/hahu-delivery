d4d63e8d0b4ba86de6f4ffce2007b39d29d7babe
Endekalu-Zemenu <endekaluzemenu2134@gmail.com>
2024-09-14 11:16:34 +0300

    add react-native-pager-view

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,25 @@
-import { Link } from 'expo-router'
 import { View, Text, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import PagerView from 'react-native-pager-view';
 
 const index = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Landing Page</Text>
-        <Link href="/sign-in">Go to sign in</Link>
-      </View>
+    <SafeAreaView className='flex-1'>
+      {/* <View>
+        <Text>Hello</Text> */}
+      <PagerView className='flex-1' initialPage={0}>
+        <View key="1" className='items-center justify-center'>
+          <Text>First page</Text>
+          <Text>Swipe ➡️</Text>
+        </View>
+        <View className='items-center justify-center' key="2">
+          <Text>Second page</Text>
+        </View>
+        <View className='items-center justify-center' key="3">
+          <Text>Third page</Text>
+        </View>
+      </PagerView>
+      {/* </View> */}
     </SafeAreaView>
   )
 }
